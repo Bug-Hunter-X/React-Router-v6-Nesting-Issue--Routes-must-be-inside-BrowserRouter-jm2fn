980ad97c29d65a0b6628b15eb502b51502613309
BugHunter-X <askzenai@gmail.com>
2025-01-08 13:42:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,18 @@
+```javascript
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users/:id" element={<User />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+```
+The solution shows the correct nesting of the `Routes` component within the `BrowserRouter` component.  This fixes the navigation issue.
